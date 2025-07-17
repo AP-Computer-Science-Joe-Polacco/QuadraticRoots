416a4aa7f9369295041c487a4e81ffdf237ad922
jpolacco <jpolacco@gmail.com>
2025-07-17 13:48:52 -0700

    Update QuadraticSolver.java

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -11,6 +11,10 @@ public class QuadraticSolver {
      * returns the roots of ax^2 + bx + c = 0 in the format:
      * "roots are r1 and r2"
      * (where r1 >= r2)
+     * Round the roots to one decimal place.
+     * You may only use arithemetic operations and type casting for the rounding.
+     * You may NOT use any rounding function, such as Math.random() nor 
+     * String.format() nor the BigDecimal class, for example. 
      * 
      * if the roots are non-real, returns "no real roots"
      * 
