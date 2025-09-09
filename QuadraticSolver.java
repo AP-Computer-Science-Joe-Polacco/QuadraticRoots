e872c71c89278634bb223f9c0ba8264c7cedb87c
jpolacco <jpolacco@gmail.com>
2025-09-09 09:51:42 -0700

    Update QuadraticSolver.java

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -13,15 +13,15 @@ public class QuadraticSolver {
      * (where r1 >= r2)
      * Round the roots to one decimal place.
      * You may only use arithemetic operations and type casting for the rounding.
-     * You may NOT use any rounding function, such as Math.random() nor 
+     * You may NOT use any rounding function, such as Math.round() nor 
      * String.format() nor the BigDecimal class, for example. 
      * HINT: For example, if r1 = 3.1415 then r1 + 0.05 = 3.1915
      * Notice how the tenths place stays where it should.
      * If r1 = 3.1515 then r1 + 0.05 = 3.2015 and the tenths place
      * appropriately adjusts. We are not done however. We need to 
      * get rid of all the other digits past the tenths place.
-     * Think about how you could use multiplication and integer division
-     * to accomplish this. 
+     * Think about how you might use multiplication, division,
+     * and/or type casting to accomplish this. 
      * 
      * if the roots are non-real, returns "no real roots"
      * 
